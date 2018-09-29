e97b7648e77c71d2d21bbd1f26dc2e1aa6d069f6
Roma <romansipyak@gmail.com>
2018-09-29 20:56:13 +0300

    add  styles for devise forms and correct some mistakes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:%i[username, email, password, password_confirmation,city_id, remember_me ])
-    devise_parameter_sanitizer.permit(:sign_in, keys:%i[login, username, email, password, remember_me])
-    devise_parameter_sanitizer.permit(:sign_out, keys:%i[username, email, password, password_confirmation, city_id, current_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :city_id, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :username, :email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_out, keys: [:email, :password, :password_confirmation, :current_password])
   end
 end
