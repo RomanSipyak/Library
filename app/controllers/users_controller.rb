2cebd57ee1d1d7cd883d7c81000b135c78f8dbf7
Roma <romansipyak@gmail.com>
2018-10-09 23:51:42 +0300

    add user show debtor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,37 @@
 class UsersController < ApplicationController
-  def new
+  def new;
   end
 
   def index
+    @users_debtor = {}
     @users = User.all
-  end
+    @users.each do |user|
+      @users_debtor[user] = if user.bookings.by_status(:owed).count > 0
+                              'Yes'
+                            else
+                              'No'
+                            end
+    end
 
-  def edit
-  end
+    def edit;
+    end
 
-  def update
-  end
+    def update;
+    end
 
-  def show
-    @user = User.find(params[:id])
-  end
+    def show
+      @user = User.find(params[:id])
+      @debtor = if @user.bookings.by_status(:owed).count > 0
+                  'Yes'
+                else
+                  'No'
+                end
+    end
 
-  def create
-  end
+    def create;
+    end
 
-  def destroy
+    def destroy;
+    end
   end
 end
