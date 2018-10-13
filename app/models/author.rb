e6320988b8aeac1f97917cf54a2bc8da4eea438f
Roma <romansipyak@gmail.com>
2018-10-13 18:10:29 +0300

    add shrine to  initializers2

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,5 @@ class Author < ApplicationRecord
   has_many :books, dependent: :nullify
   validates :name, presence: true, uniqueness: true
   validates :biography, presence: true
-
   scope :by_name, ->(by_name) {where(arel_table[:by_name].matches("%#{by_name}%"))}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_144206) do
+ActiveRecord::Schema.define(version: 2018_10_13_145051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_10_13_144206) do
     t.string "name", null: false
     t.text "biography"
     t.integer "books_count", default: 0
+    t.text "image_data"
     t.index ["name"], name: "index_authors_on_name", unique: true
   end
 
